7cdf8a04232d189397f496bb1ababceab231ba78
hadi-ilies.bereksi-reguig <hadi-ilies.bereksi-reguig@epitech.eu>
2018-03-28 19:32:14 +0200

    hit_function + jojo

---
+++ src/map_editor/hit_functions.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2017
+** my_cook
+** File description:
+** hit_functions.c
+*/
+
+
+#include <stdbool.h>
+#include "prototype.h"
+
+bool hit_point_rect(sfVector2i *point, sfFloatRect *rect)
+{
+	int nb = 0;
+
+	point->x >= rect->left ? nb++ : 0;
+	point->y >= rect->top ? nb++ : 0;
+	point->x < rect->left + rect->width ? nb++ : 0;
+	point->y < rect->top + rect->height ? nb++ : 0;
+	return (nb == 4 ? true : false);
+}
+
+bool hit_point_sprite(sfVector2i *point, sfSprite *sprite)
+{
+	sfFloatRect rect = sfSprite_getGlobalBounds(sprite);
+
+	return (hit_point_rect(point, &rect));
+}
