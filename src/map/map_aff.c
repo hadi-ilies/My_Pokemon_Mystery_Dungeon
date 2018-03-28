374d559c3c8cc37e32a475251195cef304f1277d
corentin.gautier <corentin.gautier@localhost.localdomain>
2018-03-28 09:27:41 +0200

    for norme

--- src/map/map_aff.c
+++ src/map/map_aff.c
@@ -10,12 +10,17 @@
 void map_aff(sfRenderWindow *window, map_t *map, sfVector2f *pos)
 {
 	sfVector2u win_size = sfRenderWindow_getSize(window);
-	sfVector2f posi2 = {win_size.x / 2, win_size.y / 2};
+	sfVector2f center = {win_size.x / 2, win_size.y / 2};
+	tile_map_t *tile_map = &map->tile_map;
 
 	for (size_t i = 0; i < map->nb_case_x; i++)
 		for (size_t j = 0; j < map->nb_case_y; j++) {
-			sfFloatRect rect = {map->size.x * (i - pos->x) + posi2.x, map->size.y * (j - pos->y) + posi2.y, map->size.x, map->size.y};
+			sfFloatRect rect;
 
-			tile_map_aff(window, &map->tile_map, map->tab[i][j], rect);
+			rect.left = map->size.x * (i - pos->x) + center.x;
+			rect.top = map->size.y * (j - pos->y) + center.y;
+			rect.width = map->size.x;
+			rect.height = map->size.y;
+			tile_map_aff(window, tile_map, map->tab[i][j], rect);
 		}
 }
