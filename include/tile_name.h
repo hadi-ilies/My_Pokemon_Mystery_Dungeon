b8d587a68c9a2b4d17556e3cf6820862c0996fe9
corentin.gautier <corentin.gautier@localhost.localdomain>
2018-03-29 12:08:51 +0200

    add tile_name.h

---
+++ include/tile_name.h
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2018
+** my_rpg_2017
+** File description:
+** tile_name.h
+*/
+
+#ifndef TILE_NAME_H_
+#define TILE_NAME_H_
+
+enum type_name {
+	WALL,
+	GROUND,
+	WATER
+};
+
+enum type_var {
+	V000_0X1_011,
+	V000_1X1_111,
+	V000_1X0_110,
+	V011_0X1_011,
+	V111_1X1_111,
+	V110_1X0_110,
+	V011_0X1_000,
+	V111_1X1_000,
+	V110_1X0_000,
+
+	V000_0X1_010,
+	V000_1X1_000,
+	V000_1X0_010,
+	V010_0X0_010,
+};
+
+#endif
