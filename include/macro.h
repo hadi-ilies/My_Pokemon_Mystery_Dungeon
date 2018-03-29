89987fd7de0669f2459efc28050952fe4f18616d
corentin.gautier <corentin.gautier@localhost.localdomain>
2018-03-29 09:20:16 +0200

    add marco.h

---
+++ include/macro.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** my_rpg_2017
+** File description:
+** macro.h
+*/
+
+#ifndef MACRO_H_
+#define MACRO_H_
+
+#define MOUSE_POS sfMouse_getPositionRenderWindow(window)
+
+#endif
