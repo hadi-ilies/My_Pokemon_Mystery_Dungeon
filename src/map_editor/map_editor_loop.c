e29ef42b121dda3c34e1bf71745aa06004bc69a1
corentin.gautier <corentin.gautier@localhost.localdomain>
2018-04-03 09:40:49 +0300

    modif

--- src/map_editor/map_editor_loop.c
+++ src/map_editor/map_editor_loop.c
@@ -49,8 +49,11 @@ void display_tools(sfRenderWindow *window, map_t *map, tva_t *mouse_tva)
 bool isit_on_inventory(map_t *map, sfRenderWindow *window)
 {
 	sfVector2u win_size = sfRenderWindow_getSize(window);
+	sfVector2i coord_mouse = sfMouse_getPositionRenderWindow(window);
 	sfVector2f origin;
 
+	if (sfMouse_isButtonPressed(sfMouseLeft))
+		return (false);
 	origin.x = win_size.x / 2 - map->tile_map->nb_type / 2 * TOOL_DIST;
 	origin.y = win_size.y - TOOL_SIZE / 2;
 	for (size_t i = 0; i < map->tile_map->nb_type; i++) {
@@ -58,10 +61,7 @@ bool isit_on_inventory(map_t *map, sfRenderWindow *window)
 
 		rect.left = origin.x + i * TOOL_DIST;
 		rect.top = origin.y;
-		sfVector2i coord_mouse = sfMouse_getPositionRenderWindow(window);
-		bool tmp = sfMouse_isButtonPressed(sfMouseLeft);
-
-		if (tmp && hit_point_rect(&coord_mouse, &rect))
+		if (hit_point_rect(&coord_mouse, &rect))
 			return (true);
 	}
 	return (false);
