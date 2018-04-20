c85b617ca6edece1abf087a97039ff7a9e0a076f
Corentin GAUTIER <corentin.gautier@epitech.eu>
2018-04-20 12:14:15 +0300

    auto aff

--- src/map/map_aff.c
+++ src/map/map_aff.c
@@ -12,9 +12,13 @@ void map_aff(sfRenderWindow *window, map_t *map)
 	sfVector2u win_size = sfRenderWindow_getSize(window);
 	sfVector2f center = {win_size.x / 2, win_size.y / 2};
 	tile_map_t *tile_map = map->tile_map;
+	size_t i_min = X_MIN;
+	size_t i_max = X_MAX;
+	size_t j_min = Y_MIN;
+	size_t j_max = Y_MAX;
 
-	for (size_t i = 0; i < map->nb_case_x; i++)
-		for (size_t j = 0; j < map->nb_case_y; j++) {
+	for (size_t i = i_min; i < i_max; i++)
+		for (size_t j = j_min; j < j_max; j++) {
 			sfFloatRect rect;
 
 			rect.left = map->size.x * (i - map->pos.x) + center.x;
