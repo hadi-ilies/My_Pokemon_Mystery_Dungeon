45967e39ff1a6478738fbd734347fe90cdd75a9d
hadi-ilies.bereksi-reguig <hadi-ilies.bereksi-reguig@epitech.eu>
2018-04-25 21:47:55 +0200

    create_settings

---
+++ src/main_menu/create_settings.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2018
+** create settings
+** File description:
+** create setings
+*/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "settings.h"
+
+settings_t create_setting(void)
+{
+	settings_t settings;
+
+	settings.window_size = (sfVector2u) {1920, 1080};
+	settings.sound_volume = 100;
+	settings.music_volume = 100;
+}
