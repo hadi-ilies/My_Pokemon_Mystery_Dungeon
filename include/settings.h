fec941ad4d34c08249a22b73eed168c73ddc3071
Corentin GAUTIER <corentin.gautier@epitech.eu>
2018-04-25 11:54:36 +0300

    add settings.h

---
+++ include/settings.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** my_rpg_2017
+** File description:
+** settings.h
+*/
+
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+
+typedef struct {
+	float music_volume;
+	float sound_volume;
+} settings_t;
+
+#endif
