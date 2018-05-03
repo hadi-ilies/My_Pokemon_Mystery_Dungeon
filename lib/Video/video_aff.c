1471ade5f5ba42876eee626554f60fb1e85a2724
hadi-ilies.bereksi-reguig <hadi-ilies.bereksi-reguig@epitech.eu>
2018-05-03 16:40:12 +0200

    video lib

--- lib/Video/video_aff.c
+++ lib/Video/video_aff.c
@@ -5,7 +5,30 @@
 ** video_aff.c
 */
 
+#include <stdbool.h>
 #include "video.h"
+
+bool video_create_texture(video_t *video)
+{
+	char *str = video->texture_name[video->num];
+
+	if (video->num >= video->nb_texture)
+		return (true);
+	video->texture = sfTexture_createFromFile(str, NULL);
+	if (video->texture == NULL) {
+		video->error = ERR_TEXTURE_CREATE;
+		return (true);
+	}
+	return (false);
+}
+
+void time_video(video_t *video, size_t time)
+{
+	if (time > video->time / video->nb_texture) {
+		sfClock_restart(video->clock);
+		video->num++;
+	}
+}
 
 void video_aff(sfRenderWindow *window, video_t *video, sfFloatRect rect)
 {
@@ -16,26 +39,17 @@ void video_aff(sfRenderWindow *window, video_t *video, sfFloatRect rect)
 	sfTime struct_time = sfClock_getElapsedTime(video->clock);
 	size_t time = struct_time.microseconds;
 
-	if (video->num >= video->nb_texture)
+	if (video_create_texture(video))
 		return;
-	video->texture = sfTexture_createFromFile(video->texture_name[video->num], NULL);
-	if (video->texture == NULL) {
-		video->error = ERR_TEXTURE_CREATE;
-		return;
-	}
 	sfSprite_setTexture(sprite, video->texture, sfTrue);
 	bounds = sfSprite_getLocalBounds(sprite);
 	scale.x = rect.width / bounds.width;
 	scale.y = rect.height / bounds.height;
-	origin.x = bounds.width / 2;
-	origin.y = bounds.height / 2;
+	origin = (sfVector2f){bounds.width / 2, bounds.height / 2};
 	sfSprite_setScale(sprite, scale);
 	sfSprite_setOrigin(sprite, origin);
 	sfSprite_setPosition(sprite, (sfVector2f){rect.left, rect.top});
 	sfRenderWindow_drawSprite(window, sprite, NULL);
-	if (time > video->time / video->nb_texture) {
-		sfClock_restart(video->clock);
-		video->num++;
-	}
+	time_video(video, time);
 	sfTexture_destroy(video->texture);
 }
